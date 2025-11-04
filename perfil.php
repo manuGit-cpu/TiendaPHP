8cfe7c8f68f495cf827c9eed180300f36a958f9f
manuGit-cpu <mrodriguezmendez05@gmail.com>
2025-11-04 19:53:27 +0100

    Agrega funcionalidad para actualizar el perfil de usuario, incluyendo validación de campos y mejora en el estilo del formulario.

--- perfil.php
+++ perfil.php
@@ -16,24 +16,24 @@ $perfil = obtenerPerfil();
     <form action="" method="post">
         <label for="usuario">Usuario:</label>
         <input type="text" id="usuario" name="usuario" value="<?= htmlspecialchars($perfil['usuario'] ?? '') ?>"
-            required>
+            >
 
         <label for="contraseña">Nueva Contraseña:</label>
         <input type="password" id="contraseña" name="contraseña" placeholder="Introduce una nueva contraseña">
 
 
         <label for="email">Email:</label>
-        <input type="email" id="email" name="email" value="<?= htmlspecialchars($perfil['email'] ?? '') ?>" required>
+        <input type="email" id="email" name="email" value="<?= htmlspecialchars($perfil['email'] ?? '') ?>" >
 
         <label for="fecha_nacimiento">Fecha de Nacimiento:</label>
         <input type="date" id="fecha_nacimiento" name="fecha_nacimiento" value="<?= htmlspecialchars(
             isset($perfil['fecha_nacimiento'])
             ? date('Y-m-d', strtotime($perfil['fecha_nacimiento']))
             : ''
-        ) ?>" required>
+        ) ?>" >
 
 
-        <input type="submit" value="Actualizar Perfil">
+        <button  name="actualizarPerfil" value="Actualizar Perfil"> Actualizar Perfil </button>
     </form>
 </body>
 
--- perfil/funcionesPerfil.php
+++ perfil/funcionesPerfil.php
@@ -20,5 +20,54 @@ function obtenerPerfil()
     return $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
+function actualizarPerfil(){
+    global $conexion;
+
+    if (!isset($_SESSION['id'])) {
+        header('Location: ./index.php', true, 303);
+    }
+
+    if($_POST['usuario'] == '') {
+        $_POST['usuario'] = null;
+    }
+
+    if($_POST['contraseña'] == '') {
+        $_POST['contraseña'] = null;
+    }else{
+        $hash = password_hash($_POST["contraseña"], PASSWORD_DEFAULT);
+    }
+
+    if($_POST['email'] == '') {
+        $_POST['email'] = null;
+    }
+
+    if($_POST['fecha_nacimiento'] == '') {
+        $_POST['fecha_nacimiento'] = null;
+    }
+
+    $sql = "
+    UPDATE usuarios
+    SET 
+        usuario = COALESCE(?, usuario),
+        password = COALESCE(?, password),
+        email = COALESCE(?, email),
+        fecha_nacimiento = COALESCE(?, fecha_nacimiento)
+    WHERE id = ?";
+
+    $stmt = $conexion->prepare($sql);
+
+    $stmt->bindParam(1, $_POST['usuario']);
+    $stmt->bindParam(2, $hash);
+    $stmt->bindParam(3, $_POST['email']);
+    $stmt->bindParam(4, $_POST['fecha_nacimiento']);
+    $stmt->bindParam(5, $_SESSION['id']);
+
+    $stmt->execute();
+
+}
+
+if (isset($_POST['actualizarPerfil'])) {
+    actualizarPerfil();
+}
 
 ?>
